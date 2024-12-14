f8d3eaa5977f3848bc39077f74c8152027e59711
DylanO9 <dylan.olivares11@gmail.com>
2024-12-14 03:10:37 -0800

    Simple resume design implemented

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <Intro />
-      <Projects />
       <Resume />
+      <Projects />
     </div>
   );
 }
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,15 +1,15 @@
 import resume from '../assets/Resume.pdf';
-import { FaFileAlt } from 'react-icons/fa';
+import { FaDownload } from 'react-icons/fa';
+import ariatCard from '../assets/ariat-card.png';
 import '../styles/resume.css';
 
 export default function Resume () {
     return (
         <section id="resume">
-            <h1>Work</h1>
             <ul id="work-experience-list">
                 <li className="work-experience-item">
-                    <h3>Ecommerce It Intern</h3>
-                    <h4>Ariat</h4>
+                    <h3>Ariat</h3>
+                    <h4>Ecommerce It Intern</h4>
                     <p>June 2024 - September 2024</p>
                     <ul>
                         <li>Collaborated with Business Systems Analysts to gather and document requirements for projects including athlete vouchers, UK e-gift cards, and post-purchase upsell features.</li>
@@ -18,18 +18,17 @@ export default function Resume () {
                     </ul>
                 </li>
                 <li className="work-experience-item">
-                    <h3>Developer Intern</h3>
-                    <h4>Venture Shares</h4>
+                    <h3>Venture Shares</h3>
+                    <h4>Developer Intern</h4>
                     <p>November 2023 - March 2024</p>
                     <ul>
                         <li>Utilized Next and Tailwind CSS to create responsive and visually appealing front-end components, ensuring compatibility across various devices</li>
-                        <li>Implemented user authentication and authorization using Auth0, enhancingsecurity and providing seamless access control.</li>
-                        <li>Developed a watchlist feature allowing users to monitortheir portfolio, linking profiles to stored data and providing real-time updates.</li>
+                        <li>Implemented user authentication and authorization using Auth0, enhancing security and providing seamless access control.</li>
+                        <li>Developed a watchlist feature allowing users to monitor their portfolio, linking profiles to stored data and providing real-time updates.</li>
                     </ul>
                 </li>
             </ul>
-
-            <a id="resume-download" download="Dylan_Olivares_Resume.pdf" href={resume} alt="Dylan Olivares Resume"><FaFileAlt /></a>
+            <a id="resume-download" download="Dylan_Olivares_Resume.pdf" href={resume} alt="Dylan Olivares Resume"><FaDownload size={64}/><span>Download CV</span></a>
         </section>
     );
 }
