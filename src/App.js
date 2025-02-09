d924d65e83cb8e8e2ef259a1d938115573cac1a8
DylanO9 <114462093+dolivares11@users.noreply.github.com>
2025-02-09 02:49:23 -0800

    Awards section moved, projects section updated with Fitness Network

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
       <Intro />
       <Resume />
-      <Awards />
       <Projects />
+      <Awards />
     </div>
   );
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,51 +2,73 @@ import '../styles/projects.css';
 import cryptotracker from '../assets/crypto-tracker.png';
 import flutterSafety from '../assets/flutter-safety.jpg';
 import palmReading from '../assets/palm-reading.jpg';
+import iphone1 from '../assets/iphone_1.png';
+import iphone2 from '../assets/iphone_2.png';
+import iphone3 from '../assets/iphone_3.png';
+import iphone4 from '../assets/iphone_4.png';
+import iphone5 from '../assets/iphone_5.png';
+import iphone6 from '../assets/iphone_6.png';
+import iphone7 from '../assets/iphone_7.png';
 
 import { FaGithub } from 'react-icons/fa';
 
 export default function Projects () {
+    const projects = [
+        {
+            title: "Fitness Network",
+            technologies: ["Flutter"],
+            images: [iphone1, iphone6, iphone7, iphone2, iphone3, iphone5],
+            githubLink: "https://github.com/DylanO9/fitness_network.git"
+        },
+        {
+            imgSrc: cryptotracker,
+            imgAlt: "Crypto tracker project",
+            title: "Crypto Tracker",
+            technologies: ["React", "Javascript", "CSS", "Firebase"],
+            githubLink: "https://github.com/DylanO9/Crypto-Tracker"
+        },
+        {
+            imgSrc: palmReading,
+            imgAlt: "Palm Reading Project",
+            title: "Palm Reading",
+            technologies: ["HTML", "Javascript", "CSS"],
+            githubLink: "https://github.com/cse110-sp23-group3/cse110-sp23-group3",
+            liveLink: "https://cse110-sp23-group3.github.io/cse110-sp23-group3/source/"
+        },
+        {
+            imgSrc: flutterSafety,
+            imgAlt: "Flutter safety project",
+            title: "Flutter Safety",
+            technologies: ["Flutter"],
+            githubLink: "https://github.com/Renzo-Olivares/flutter_virus"
+        }
+    ];
+
     return (
         <section id="projects">
             <ul>
-                <li>
-                    <img src={cryptotracker} className="project-image" alt="Crypto tracker project"></img>
-                    <h3>Crypto Tracker</h3>
-                    <ul className="technologies-list">
-                        <li className="technologies-item">React</li>
-                        <li className="technologies-item">Javascript</li>
-                        <li className="technologies-item">CSS</li>
-                        <li className="technologies-item">Firebase</li>
-                    </ul>
-                    <a href="https://github.com/DylanO9/Crypto-Tracker" className="projects-github" target="_blank" rel="noopener noreferrer"><FaGithub size={32}/></a>
-                </li>
-                <li>
-                    <img src={palmReading} className="project-image" alt="Palm Reading Project"></img>
-                    <h3>Palm Reading</h3>
-                    <ul className="technologies-list">
-                        <li className="technologies-item">HTML</li>
-                        <li className="technologies-item">Javascript</li>
-                        <li className="technologies-item">CSS</li>
-                    </ul>
-                    <a href="https://github.com/cse110-sp23-group3/cse110-sp23-group3" className="projects-github" target="_blank" rel="noopener noreferrer"><FaGithub size={32}/></a>
-                </li>
-                <li>
-                    <img src={flutterSafety} className="project-image" alt="Flutter safety project"></img>
-                    <h3>Flutter Safety</h3>
-                    <ul className="technologies-list">
-                        <li className="technologies-item">Flutter</li>
-                    </ul>
-                    <a href="https://github.com/Renzo-Olivares/flutter_virus" className="projects-github" target="_blank" rel="noopener noreferrer"><FaGithub size={32}/></a>
-                </li>
-                <li>
-                    <h3>Project 4</h3>
-                    <ul className="technologies-list">
-                        <li className="technologies-item">React</li>
-                        <li className="technologies-item">Javascript</li>
-                        <li className="technologies-item">CSS</li>
-                        <li className="technologies-item">SQL</li>
-                    </ul>
-                </li>
+                {projects.map((project, index) => (
+                    <li key={index}>
+                        {project.images && (
+                            <div className="image-grid">
+                                {project.images.map((image, imgIndex) => (
+                                    <img key={imgIndex} src={image} className="grid-image" alt={`iPhone ${imgIndex + 1}`}></img>
+                                ))}
+                            </div>
+                        )}
+                        {project.imgSrc && <img src={project.imgSrc} className="project-image" alt={project.imgAlt}></img>}
+                        <h3>{project.title}</h3>
+                        <ul className="technologies-list">
+                            {project.technologies.map((tech, techIndex) => (
+                                <li key={techIndex} className="technologies-item">{tech}</li>
+                            ))}
+                        </ul>
+                        <ul className="project-links">
+                            {project.githubLink && <a href={project.githubLink} className="projects-github" target="_blank" rel="noopener noreferrer"><FaGithub size={32}/></a>}
+                            {project.liveLink && <a href={project.liveLink} className="projects-live" target="_blank" rel="noopener noreferrer"><span className="live-button">Live</span></a>}
+                        </ul>
+                    </li>
+                ))}
             </ul>
         </section>
     );
